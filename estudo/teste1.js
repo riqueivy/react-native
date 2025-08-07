ebae447330445946b0548169ff36ea56a5d7a1e0
riqueivy <riqueivymeneses@gmail.com>
2025-08-07 17:53:40 -0300

    novas atualizaçoes dia 07/08

--- estudo/teste1.js
+++ estudo/teste1.js
@@ -1,6 +1,6 @@
 // props--> propriedades de projeto
 import React, {Component} from "react";
-import { View, Button, Image, Text} from "react-native";
+import { View, Button, Image, Text, StyleSheet } from "react-native";
 class app extends Component{
     render(){
 
@@ -11,6 +11,10 @@ class app extends Component{
             <View>
             <text style={{ color:'red', fontSize: 12, margin: 12}}>ola mundo</text>
             <text>{nome}</text>
+
+            <text style={[estilo.texto]}></text>
+            <text style={[estilo.area, estilo.texto]}></text>
+
             <Image source={{ uri: img}} style={{ width:20 , height:20 }}/>
 
             <Button title="clicar" onPress={app}/>
@@ -25,3 +29,22 @@ function app(){
 
     
 }
+//bloco CSS
+const estilo = StyleSheet.create({
+    area:{
+        marginTop: 40
+    },
+
+    texto: {
+        fontSize: 25,
+        color: '#FF0000'
+
+    },
+
+    fundo:{
+        textAlign: 'center',
+        backgroundColor: '#99CC32'
+
+    }
+})
+ 
